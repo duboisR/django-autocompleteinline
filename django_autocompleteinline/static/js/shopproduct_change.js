53635b71d107635086dd6a3312aaaf1c36ebed9f
DUBOIS Romain <dubois.rom@gmail.com>
2018-03-03 22:39:02 +0100

    Replace MutationEvent with MutationObserver

--- django_autocompleteinline/static/js/shopproduct_change.js
+++ django_autocompleteinline/static/js/shopproduct_change.js
@@ -6,17 +6,27 @@
       $(`.dynamic-${inline}`).each(function(index) {
         var span = $(`#select2-id_${inline}-${index}-product-container`);
         var price_input = $(`#id_${inline}-${index}-price`);
-        span.bind('DOMNodeInserted', function() {
-          var product_name = span[0].firstChild.data;
-          $.ajax({
-            'type'     : 'GET',
-            'url'      : `/api/product_price/?product_name=${product_name}`,
-            'dataType' : 'json',
-            'cache'    : false,
-            'success'  : function(json) {
-              price_input[0].value = json['price'];
-            }
-          });
+
+        var observer = new MutationObserver(function(mutation) {
+          var newNodes = mutation.addedNodes; // DOM NodeList
+          if( newNodes !== null ) { // If there are new nodes added
+            var product_name = span[0].firstChild.data;
+            $.ajax({
+              'type'     : 'GET',
+              'url'      : `/api/product_price/?product_name=${product_name}`,
+              'dataType' : 'json',
+              'cache'    : false,
+              'success'  : function(json) {
+                price_input[0].value = json['price'];
+              }
+            });
+          }
+        });
+        observer.observe(span[0], {
+          childList: true,
+          subtree: false,
+          attributes: false,
+          characterData: false,
         });
       });
     }
